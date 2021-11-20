1d812ff9a2c67457f713a309e7daeab10388a40c
deejayy <deejayy@deejayy.hu>
2021-11-20 13:16:40 +0100

    Project setup

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: "jest-preset-angular",
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.spec.json",
+      stringifyContentPathRegex: "\\.html$",
+      diagnostics: false,
+    },
+  },
+  setupFilesAfterEnv: ["<rootDir>/src/setupJest.ts"],
+  moduleDirectories: ["node_modules", "src"],
+  moduleNameMapper: {
+    "@app/(.*)": "<rootDir>/src/app/$1",
+    "@core/(.*)": "<rootDir>/src/app/core/$1",
+    "@feature/(.*)": "<rootDir>/src/app/feature/$1",
+    "@shared/(.*)": "<rootDir>/src/app/shared/$1",
+    "@env/(.*)": "<rootDir>/src/environments/$1",
+  },
+};
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PROJECTS_ROUTES } from './feature/projects/projects-routing-parent';
+
+const routes: Routes = [...PROJECTS_ROUTES];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
