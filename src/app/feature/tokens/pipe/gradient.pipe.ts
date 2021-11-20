ca8b6959af9f4337de469d3367fa8cc45e7c3ae7
deejayy <deejayy@deejayy.hu>
2021-11-20 23:11:42 +0100

    Adjust gradient

--- src/app/feature/tokens/pipe/gradient.pipe.ts
+++ src/app/feature/tokens/pipe/gradient.pipe.ts
@@ -4,6 +4,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class GradientPipe implements PipeTransform {
   transform(value: string): unknown {
     const crc = Array.from({ length: value.length }, (_, i) => value.charCodeAt(i)).reduce((acc, curr) => acc + curr, 0) * value.length % 360;
-    return `linear-gradient(165deg, hsl(${crc}, 100%, 70%), hsl(${crc + 45}, 100%, 70%))`;
+    return `linear-gradient(165deg, hsl(${crc}, 100%, 60%), hsl(${crc + 45}, 80%, 50%))`;
   }
 }
