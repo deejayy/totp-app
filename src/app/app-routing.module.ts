f18d5be205830ea51d32741b1fff1b32359b40fd
deejayy <deejayy@deejayy.hu>
2021-11-21 14:59:37 +0100

    Forgot about the default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { TOKENS_ROUTES } from './feature/tokens/tokens-routing-parent';
 
-const routes: Routes = [...TOKENS_ROUTES];
+const routes: Routes = [
+  ...TOKENS_ROUTES,
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/tokens',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
